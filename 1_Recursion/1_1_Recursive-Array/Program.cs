3c2e162048109b5bec05ccdbe0fcab84c352a630
Lachezar Lalov <luchoazz@abv.bg>
2017-09-05 01:24:16 +0300

    initial commit

---
+++ 1_Recursion/1_1_Recursive-Array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_1_Recursive_Array
+{
+    class Program
+    {
+        static int Sum(int[] array, int index)
+        {
+            if(index == array.Length - 1)
+            {
+                return array[index];
+            }
+            return array[index] + Sum(array, index + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+            Console.WriteLine(Sum(input, 0));
+        }
+    }
+}
