45b3c9a11dd4f0d0fd434876b2c45b5e067b2e3d
Lachezar Lalov <luchoazz@abv.bg>
2017-09-05 10:03:40 +0300

    added simple recursive console drawing

---
+++ 1_Recursion/1_3_Recursive-Drawing/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1_3_Recursive_Drawing
+{
+    class Program
+    {
+        public static void Draw(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', n));
+            Draw(n - 1);
+            Console.WriteLine(new string('#', n));
+        }
+
+        static void Main()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            Draw(n);
+        }
+
+        //  EXPECTED OUTPUT FOR INPUT 5:
+
+        //  *****
+        //  ****
+        //  ***
+        //  **
+        //  *
+        //  #
+        //  ##
+        //  ###
+        //  ####
+        //  #####
+       
+    }
+}
