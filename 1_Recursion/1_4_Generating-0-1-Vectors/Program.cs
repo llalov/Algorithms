86b9dfd57b7be60b3b61d11d3a773fd15d903557
Lachezar Lalov <luchoazz@abv.bg>
2017-09-10 14:12:59 +0300

    added method for generating all possbile 0-1 n-lenght vector combinations

---
+++ 1_Recursion/1_4_Generating-0-1-Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_4_Generating_0_1_Vectors
+{
+    class Program
+    {
+        //Generates all possible 0-1 combinations in by passing n-lenght vector in the console
+        //*note -  if you want to change the range of possible numbers edit the start and end numbers in the for loop below 
+        public static void Gen01(int[] vector, int index)
+        {
+            if (index > vector.Length - 1)
+            {
+                Console.WriteLine("{0}", string.Join("", vector.Select(x => x.ToString())));      
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    vector[index] = i;
+                    Gen01(vector, index + 1);
+                }
+            }
+        }
+
+        static void Main()
+        {
+            int vectorLength = Convert.ToInt32(Console.ReadLine());
+            Gen01(new int[vectorLength], 0);
+        }
+    }
+}
