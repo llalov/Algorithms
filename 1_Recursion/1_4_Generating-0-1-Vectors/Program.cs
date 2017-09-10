b97d5765acb55a567a5525828cce898eea9973c7
Lachezar Lalov <luchoazz@abv.bg>
2017-09-10 14:29:30 +0300

    added method for generating combinations  for given set and range

--- 1_Recursion/1_4_Generating-0-1-Vectors/Program.cs
+++ 1_Recursion/1_4_Generating-0-1-Vectors/Program.cs
@@ -5,7 +5,7 @@ namespace _1_4_Generating_0_1_Vectors
 {
     class Program
     {
-        //Generates all possible 0-1 combinations in by passing n-lenght vector in the console
+        //Generates all possible 0-1 combinations in by passing the lenght of the vector in the console
         //*note -  if you want to change the range of possible numbers edit the start and end numbers in the for loop below 
         public static void Gen01(int[] vector, int index)
         {
---
+++ 1_Recursion/1_5_Generating-Combinations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_5_Generating_Combinations
+{
+    class Program
+    {
+        public static void GenComb(int[]set, int[]vector, int index, int border)
+        {
+
+        }
+
+        static void Main()
+        {
+            
+        }
+    }
+}
