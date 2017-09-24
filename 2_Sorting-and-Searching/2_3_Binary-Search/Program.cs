2b98ea0acd082e230ba2a80d8aa4f80d5e6a4b12
Lachezar Lalov <luchoazz@abv.bg>
2017-09-25 00:48:19 +0300

    added binary search

---
+++ 2_Sorting-and-Searching/2_3_Binary-Search/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _2_3_Binary_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 6, 7, 9, 12, 23, 24, 33, 45, 66, 71, 87, 92, 95 };
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int index = BinarySearch.IndexOf(array, 87);
+            stopwatch.Stop();
+            Console.WriteLine("Result: "+ index);
+            Console.WriteLine("Execute in " + stopwatch.ElapsedMilliseconds.ToString() + " milliseconds");
+        }
+    }
+}
