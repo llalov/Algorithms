a2a6bf1f7f188ea92927ea45bd10cd695462aab0
Lachezar Lalov <luchoazz@abv.bg>
2017-10-01 02:32:58 +0300

    added combinations with repetitions algorithm

--- 3_Combinatorial-Algorithms/3_6_Combinations-with-repetitions/Program.cs
+++ 3_Combinatorial-Algorithms/3_6_Combinations-with-repetitions/Program.cs
@@ -4,9 +4,29 @@ namespace _3_6_Combinations_with_repetitions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const int k = 3;
+            int n = 5;
+            int[] elements = new int[k];
+            GenerateCombinations(0, 0);
+
+            //Generate combinations with repetitions for k elements in n length collection.
+            void GenerateCombinations(int index, int start)
+            {
+                if (index >= k)
+                {
+                    Console.WriteLine(String.Join(" ", elements));
+                }
+                else
+                {
+                    for (int i = start; i < n; i++)
+                    {
+                        elements[index] = i;
+                        GenerateCombinations(index + 1, i);
+                    }
+                }
+            }
         }
     }
 }
