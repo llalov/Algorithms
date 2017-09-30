8b2c2694e6d6ae5f0057ff1d80c28b21bc18ca4b
Lachezar Lalov <luchoazz@abv.bg>
2017-10-01 01:32:07 +0300

    added variations with repetitions algorithm

--- 3_Combinatorial-Algorithms/3_4_Variations-with-repetitions/Program.cs
+++ 3_Combinatorial-Algorithms/3_4_Variations-with-repetitions/Program.cs
@@ -4,9 +4,33 @@ namespace _3_4_Variations_with_repetitions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            const int k = 3;
+            int n = 5;
+            int[] elements = new int[k];
+
+            //Variations with repetitions. Iterative algorithm.
+            //Variations count: n power k. (In this case 5*5*5 = 125)
+            while (true)
+            {
+                Console.WriteLine(String.Join(" ", elements));
+                int index = k - 1;
+                while (index >= 0 && elements[index] == n - 1)
+                {
+                    index--;
+                }
+                if (index < 0)
+                {
+                    break;
+                }
+                elements[index]++;
+                for (int i = index + 1; i < k; i++)
+                {
+                    elements[i] = 0;
+                }
+            }
         }
     }
 }
