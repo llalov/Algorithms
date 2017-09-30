4327d87921f2bfe4879a8f29617efce6e0787594
Lachezar Lalov <luchoazz@abv.bg>
2017-10-01 02:24:26 +0300

    added combinations algorithm

--- 3_Combinatorial-Algorithms/3_5_Combinations-withour-repetitions/Program.cs
+++ 3_Combinatorial-Algorithms/3_5_Combinations-withour-repetitions/Program.cs
@@ -6,7 +6,28 @@ namespace _3_5_Combinations_withour_repetitions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int k = 3;
+            int n = 5;
+            int[] elements = new int[k];
+            GenerateCombinations(0, 0);
+            
+            //Generate combinations for k elements in n length collection.
+            //Combinations count: n! / k!(n-k)!
+            void GenerateCombinations(int index, int start)
+            {
+                if (index >= k)
+                {
+                    Console.WriteLine(String.Join(" ", elements));
+                }
+                else
+                {
+                    for (int i = start; i < n; i++)
+                    {
+                        elements[index] = i;
+                        GenerateCombinations(index + 1, i + 1);
+                    }
+                }
+            }
         }
     }
 }
