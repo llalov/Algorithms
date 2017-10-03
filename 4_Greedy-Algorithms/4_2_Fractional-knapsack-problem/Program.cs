d491d18296d532798cf3e79f3c8edba80c58d4d7
Lachezar Lalov <luchoazz@abv.bg>
2017-10-03 13:52:16 +0300

    initial commit

---
+++ 4_Greedy-Algorithms/4_2_Fractional-knapsack-problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_2_Fractional_knapsack_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 4_Greedy-Algorithms/4_3_Conference-scheduling-problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_3_Conference_scheduling_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
