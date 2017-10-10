4b79eb90d9d7bf08431f860ca4298ecbfc398cd0
Lachezar Lalov <luchoazz@abv.bg>
2017-10-10 23:26:30 +0300

    added solution for best way to cut up a rod with specified length.

---
+++ 5_Dynamic-Programming/5_4_Rod-Cutting/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _5_4_Rod_Cutting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] price = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+            int rodLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] bestPrice = new int[price.Length];
+            int[] bestCombo = new int[price.Length];
+            Array.Copy(price, bestPrice, price.Length);
+
+            Console.WriteLine(CutRod(rodLength));
+
+            int CutRod(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    int currentBest = bestPrice[i];
+                    for (int j = 1; j <= i; j++)
+                    {
+                        currentBest =
+                             Math.Max(bestPrice[i], price[j] + bestPrice[i - j]);
+                        if (currentBest > bestPrice[i])
+                        {
+                            bestPrice[i] = currentBest;
+                            bestCombo[i] = j;
+                        }
+                    }
+                }
+                return bestPrice[n];
+            }
+
+        }
+    }
+}
