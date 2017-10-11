1064a8f780a6280d03aa0a214c0ca1fbac97af89
Lachezar Lalov <luchoazz@abv.bg>
2017-10-12 00:43:08 +0300

    initial commit

---
+++ 6_Dynamic-Programming-Part-II/6_1_Knapsack-0-1-problem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_1_Knapsack_0_1_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
