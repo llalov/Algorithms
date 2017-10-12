1bd67dd18f61815bfd899d87c875f50d741f5a8a
Lachezar Lalov <luchoazz@abv.bg>
2017-10-12 23:29:59 +0300

    added solution

--- 6_Dynamic-Programming-Part-II/6_1_Knapsack-0-1-problem/Program.cs
+++ 6_Dynamic-Programming-Part-II/6_1_Knapsack-0-1-problem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _6_1_Knapsack_0_1_problem
 {
@@ -6,7 +7,57 @@ namespace _6_1_Knapsack_0_1_problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int maxWeight = Convert.ToInt32(Console.ReadLine());
+            List<int> weights = new List<int>();
+            List<int> values = new List<int>();
+
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                values.Add(Convert.ToInt32(input.Split(' ')[1]));
+                weights.Add(Convert.ToInt32(input.Split(' ')[0]));
+            }
+
+            int[] valuesArr = values.ToArray();
+            int[] weightsArr = weights.ToArray();
+
+            Console.WriteLine(Knapsack(maxWeight, weightsArr, valuesArr, values.Count));
+
+            // finds the maximum value of combinations items in a knapsack with limited weight
+            int Knapsack(int knapsackCapacity, int[] wt, int[] val, int countOfAllItems)
+            {
+                int[,] maxPrice = new int[countOfAllItems + 1, knapsackCapacity + 1];
+                for (int row = 0; row <= countOfAllItems; row++)
+                {
+                    for (int col = 0; col <= knapsackCapacity; col++)
+                    {
+                        if (row == 0 || col == 0)
+                        {
+                            maxPrice[row,col] = 0;
+                        }
+                        else if(wt[row - 1] <= col) 
+                        {
+                            maxPrice[row, col] = Max(val[row - 1] + maxPrice[row - 1, col - wt[row - 1]], maxPrice[row - 1, col]);
+                        }
+                        else
+                        {
+                            maxPrice[row, col] = maxPrice[row - 1, col];
+                        }
+                    }
+                }
+                return maxPrice[countOfAllItems,knapsackCapacity];
+            }
+
+            int Max(int a, int b)
+            {
+                return (a > b) ? a : b;
+            }
         }
     }
 }
