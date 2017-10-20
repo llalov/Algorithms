4f3e0e7c3a99bb79eccc06027e9a046391f012af
Lachezar Lalov <luchoazz@abv.bg>
2017-10-20 09:57:54 +0300

    initial commit

---
+++ 7_1_Graphs-and-graph-algorithms/7_1_Connected-components/GraphConnectedComponents.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7_1_Connected_components
+{
+    class GraphConnectedComponents
+    {
+        static List<int>[] graph = new List<int>[]
+     {
+        new List<int>() { 3, 6 },
+        new List<int>() { 3, 4, 5, 6 },
+        new List<int>() { 8 },
+        new List<int>() { 0, 1, 5 },
+        new List<int>() { 1, 6 },
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 1, 4 },
+        new List<int>() { },
+        new List<int>() { 2 }
+     };
+
+        public static void Main()
+        {
+            //visited = new bool[graph.Length];
+
+            //graph = ReadGraph();
+            FindGraphConnectedComponents();
+        }
+
+        private static List<int>[] ReadGraph()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var graph = new List<int>[n];
+            for (int i = 0; i < n; i++)
+            {
+                graph[i] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToList();
+            }
+            return graph;
+        }
+
+        private static void FindGraphConnectedComponents()
+        {
+            visited = new bool[graph.Length];
+            for (int startNode = 0; startNode < graph.Count(); startNode++)
+            {
+                if (!visited[startNode])
+                {
+                    Console.Write("Connected component:");
+                    DFS(startNode);
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static bool[] visited;
+
+        private static void DFS(int vertex)
+        {
+            if (!visited[vertex])
+            {
+                visited[vertex] = true;
+                foreach (var child in graph[vertex])
+                {
+                    DFS(child);
+                }
+                Console.Write(" " + vertex);
+            }
+        }
+    }
+}
