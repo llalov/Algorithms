8757be5b16025493c8508b63401512e109b5b696
Lachezar Lalov <luchoazz@abv.bg>
2017-10-23 10:01:49 +0300

    added "topological sorting" algorithm

---
+++ 7_1_Graphs-and-graph-algorithms/7_2_Topological-sorting/TopologicalSorter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private Dictionary<string, int> predecessorCount;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    public ICollection<string> TopSort()
+    {
+        List<string> sorted = new List<string>();
+        GetPredecessorCount(graph);
+        while (true)
+        {
+            string nodeToRemove = predecessorCount.Keys.Where(x => predecessorCount[x] == 0).FirstOrDefault();
+            if (nodeToRemove == null)
+                break;
+
+            foreach(var child in graph[nodeToRemove])
+                predecessorCount[child]--;
+            graph.Remove(nodeToRemove);
+            sorted.Add(nodeToRemove);
+        }
+        return sorted;
+    }
+
+    private void GetPredecessorCount(Dictionary<string, List<string>> graph)
+    {
+        predecessorCount = new Dictionary<string, int>();
+        foreach(var node in graph)
+        {
+            if (!predecessorCount.ContainsKey(node.Key))
+                predecessorCount[node.Key] = 0;
+
+            foreach(var child in node.Value)
+            {
+                if (!predecessorCount.ContainsKey(child))
+                    predecessorCount[child] = 0;
+                predecessorCount[child]++;
+            }
+        }
+    }
+}
+
