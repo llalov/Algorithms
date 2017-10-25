ce73b21e6f9895a19686aa35a881f9641f7e7089
Lachezar Lalov <luchoazz@abv.bg>
2017-10-25 10:29:10 +0300

    added DFS algorithm

--- 7_1_Graphs-and-graph-algorithms/7_2_Topological-sorting/TopologicalSorter.cs
+++ 7_1_Graphs-and-graph-algorithms/7_2_Topological-sorting/TopologicalSorter.cs
@@ -5,47 +5,57 @@ using System.Linq;
 public class TopologicalSorter
 {
     private Dictionary<string, List<string>> graph;
-    private Dictionary<string, int> predecessorCount;
-    private HashSet<string> visited; // used for DFS algorithm
+    private Dictionary<string, int> predecessorCount; // used with Iterative algorithm
+    private HashSet<string> visited = new HashSet<string>(); // used with DFS algorithm
 
     public TopologicalSorter(Dictionary<string, List<string>> graph)
     {
         this.graph = graph;
     }
 
+    // Used with DFS algorithm
     public ICollection<string> TopSort()
     {
-        List<string> sorted = new List<string>();
-        GetPredecessorCount(graph);
-        while (true)
-        {
-            string nodeToRemove = predecessorCount.Keys.Where(x => predecessorCount[x] == 0).FirstOrDefault();
-            if (nodeToRemove == null)
-                break;
-
-            foreach(var child in graph[nodeToRemove])
-                predecessorCount[child]--;
-            predecessorCount.Remove(nodeToRemove);
-            graph.Remove(nodeToRemove);
-            sorted.Add(nodeToRemove);
-        }
-        if (graph.Count > 0)
-            throw new InvalidOperationException();
+        LinkedList<string> sorted = new LinkedList<string>();
+        foreach (var node in this.graph.Keys)
+            DFS(node, sorted);
         return sorted;
     }
 
-    //You can select one of the two methonds below to traverse the graph
+    //Used with Iterative algorithm
+    //public ICollection<string> TopSort()
+    //{
+    //    List<string> sorted = new List<string>();
+    //    GetPredecessorCount(graph);
+    //    while (true)
+    //    {
+    //        string nodeToRemove = predecessorCount.Keys.Where(x => predecessorCount[x] == 0).FirstOrDefault();
+    //        if (nodeToRemove == null)
+    //            break;
 
+    //        foreach (var child in graph[nodeToRemove])
+    //            predecessorCount[child]--;
+    //        predecessorCount.Remove(nodeToRemove);
+    //        graph.Remove(nodeToRemove);
+    //        sorted.Add(nodeToRemove);
+    //    }
+    //    if (graph.Count > 0)
+    //        throw new InvalidOperationException();
+    //    return sorted;
+    //}
+
+    //You can select one of the two methonds below to traverse the graph
+    //------------------------------------------------------------------
+
+    //Used with Iterative algorithm
     private void GetPredecessorCount(Dictionary<string, List<string>> graph)
     {
         predecessorCount = new Dictionary<string, int>();
-
-        foreach(var node in graph)
+        foreach (var node in graph)
         {
             if (!predecessorCount.ContainsKey(node.Key))
                 predecessorCount[node.Key] = 0;
-
-            foreach(var child in node.Value)
+            foreach (var child in node.Value)
             {
                 if (!predecessorCount.ContainsKey(child))
                     predecessorCount[child] = 0;
@@ -53,14 +63,13 @@ public class TopologicalSorter
             }
         }
     }
-
     private void DFS(string node, LinkedList<string> result)
     {
         if (!visited.Contains(node))
         {
             visited.Add(node);
-            //TODO: visit all children with DFS
-
+            foreach (var child in graph[node])
+                DFS(child, result);
             result.AddFirst(node);
         }
     }
