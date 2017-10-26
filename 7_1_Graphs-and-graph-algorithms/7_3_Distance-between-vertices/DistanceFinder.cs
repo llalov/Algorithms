f2cef409a42f6c7180db5cfcf40505bd52cabdd6
Lachezar Lalov <luchoazz@abv.bg>
2017-10-26 10:09:02 +0300

    initial commit for finding distance between vertices

---
+++ 7_1_Graphs-and-graph-algorithms/7_3_Distance-between-vertices/DistanceFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class DistanceFinder
+{
+    private Dictionary<string, List<string>> graph;
+
+    public DistanceFinder(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+    }
+
+    static void BFS()
+    {
+        //TODO
+    }
+}
+
