d79b1a356e0673c56ca492de5788817967bd160e
Lachezar Lalov <luchoazz@abv.bg>
2017-11-17 00:16:12 +0200

    added Dijkstra algorithm

--- 8_Advanced-Graph-Algorithms-Part-I/8_2_Dijkstra/DijkstraWithoutPriorityQueue.cs
+++ 8_Advanced-Graph-Algorithms-Part-I/8_2_Dijkstra/DijkstraWithoutPriorityQueue.cs
@@ -7,7 +7,61 @@ public class DijkstraWithoutPriorityQueue
 {
     public static List<int> DijkstraAlgorithm(int[,] graph, int sourceNode, int destinationNode)
     {
-        throw new NotImplementedException();
+        int n = graph.GetLength(0);
+        int[] distance = new int[n];
+
+        for (int i = 0; i < n; i++)
+            distance[i] = int.MaxValue;
+        
+        distance[sourceNode] = 0;
+        var used = new bool[n];
+        int?[] previous = new int?[n];
+
+        while(true)
+        {
+            //Find the nearest unused node from the graph
+            int minDistance = int.MaxValue;
+            int minNode = 0;
+            for (int node = 0; node < n; node++)
+            {
+                if (!used[node] && distance[node] < minDistance)
+                {
+                    minDistance = distance[node];
+                    minNode = node;
+                }
+            }
+            if (minDistance == int.MaxValue)
+                break;
+            used[minNode] = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (graph[minNode,i] > 0)
+                {
+                    int newDist = distance[minNode] + (graph[minNode,i]);
+                    if (newDist < distance[i])
+                    {
+                        distance[i] = newDist;
+                        previous[i] = minNode;
+                        //update previous if nesscesary
+                    }
+                }
+            }
+        }
+
+        if (distance[destinationNode] == int.MaxValue)
+            return null;
+
+        //Reconstruct the shortest path from sourceNode to destinationNode
+        var path = new List<int>();
+        int? currentNode = destinationNode;
+        while (currentNode != null)
+        {
+            path.Add(currentNode.Value);
+            currentNode = previous[currentNode.Value];
+        }
+        path.Reverse();
+        return path;
     }
 }
 
